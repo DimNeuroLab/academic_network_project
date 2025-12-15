3dea3d88ffd5cc8fb5eac85565753a5a258bfd57
Sabrina Guidotti <sguidotti2001@gmail.com>
2025-12-15 10:43:23 +0100

    Move legacy code and add runner

--- shell_scipts/gen_recbole_edge.sh
+++ shell_scipts/gen_recbole_edge.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ../anp_utils
- python3 get_author_from_recbole.py 10_NAIS 4 3
- python3 get_author_from_recbole.py 10_LightCGN 5 3
+python3 get_author_from_recbole.py 10_LightGCN 5 0
 
 
 
